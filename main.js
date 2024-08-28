bc93f454dddfacf3c82dd3293ad8211c679ee2c3
menady0 <menady20@gmail.com>
2024-08-28 21:52:01 +0300

    adding 'sayHello' page

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Dark Mode
 const toggle = document.getElementById('toggleDark');
 const body = document.querySelector('body');
 const leftCard = document.getElementsByClassName('left-card');
@@ -34,3 +35,24 @@ toggle.addEventListener('click', function(){
   }
   body.style.transition = '2s'
 })
+
+
+// Testimonial Slider
+var btn = document.getElementsByClassName('i-btn');
+var slide = document.getElementById('slide');
+
+btn[0].onclick = function(){
+  slide.style.transform = 'translateX(0px)'
+  for(i = 0; i < 2; i++){
+    btn[i].classList.remove('active');
+  }
+  this.classList.add('active')
+}
+btn[1].onclick = function(){
+  slide.style.transform = 'translateX(-400px)'
+  for(i = 0; i < 2; i++){
+    btn[i].classList.remove('active');
+  }
+  this.classList.add('active')
+}
+
